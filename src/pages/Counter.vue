7dd5ce59d39378064b8467e6152d36442484fe13
takashi11171117 <takashi11171117@gmail.com>
2022-05-14 11:51:19 +0900

    confirm watch and mounted

--- src/pages/Counter.vue
+++ src/pages/Counter.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 
 const number = ref(0)
 
 const increment = () => {
   number.value++
 }
+
+watch(number, () => {
+  console.log('Number Changed')
+})
+
+onMounted(() => {
+  console.log('Component mounted')
+})
 </script>
 
 <template>
